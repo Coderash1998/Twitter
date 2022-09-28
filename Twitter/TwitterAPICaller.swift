d26ee82292f3f5d47bcadeeafe9e52e96f2dc095
Coderash1998 <47770728+Coderash1998@users.noreply.github.com>
2022-09-28 14:42:06 -0500

    Changed API key

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "3QbxyqHVBXKFaiU8UwapmVvsJ", consumerSecret: "hYCCTdJBA4J9paFqFy7GseYMIVNvvEdd9V4IcsQVmC4lo8UlxK")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
